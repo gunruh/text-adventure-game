cfb3da4e7cbac59aa3cbd229a18e19301dc949e5
Grant Unruh - Phone <grantunruh640@gmail.com>
2019-12-29 22:22:52 -0600

    renaming luxury dresser to avoid confusion.

--- src/main/java/com/gunruh/textgame/objects/rooms/starship/level1/PassengersQuarters.java
+++ src/main/java/com/gunruh/textgame/objects/rooms/starship/level1/PassengersQuarters.java
@@ -7,7 +7,7 @@ public class PassengersQuarters extends Room {
     private PassengersQuarters() {
         super("Passenger's Quarters", "A nice-looking room for passengers to live in.");
         getAvailableObjects().add(new GameObject("Luxury Bed", "It looks cozy, but you shouldn't sleep here.") {});
-        getAvailableObjects().add(new GameObject("Luxury Dresser", "Hand-carved dresser from space trees.") {});
+        getAvailableObjects().add(new GameObject("Fancy Dresser", "Hand-carved dresser from space trees.") {});
     }
 
     private static PassengersQuarters INSTANCE = new PassengersQuarters();
