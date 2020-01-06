65b6228cd0da2a9e99b6678dd0f09c842e646bd9
Grant Unruh - Phone <grantunruh640@gmail.com>
2020-01-06 11:41:29 -0600

    adding AnderdonJr class to git.

---
+++ src/main/java/com/gunruh/textgame/objects/rooms/characters/AndersonJr.java
@@ -0,0 +1,31 @@
+package com.gunruh.textgame.objects.rooms.characters;
+
+import com.gunruh.textgame.objects.GameObject;
+import com.gunruh.textgame.objects.Player;
+import com.gunruh.textgame.utils.IOUtils;
+
+public class AndersonJr extends GameObject {
+	private AndersonJr() {
+		super("Captain's Son", "The son of the captain. He is still earning his junior flying badge.");
+		setNickName("Billy Anderson Jr.");
+	}
+	
+	private static AndersonJr INSTANCE = new AndersonJr();
+	
+	public static AndersonJr getInstance() {
+		return INSTANCE;
+	}
+	
+	@Override
+	public void receiveTalkTo(GameObject gameObject) {
+		if (Player.getInstance().getCurrentRoom().getAvailableObjects().contains(Captain.getInstance())) {
+			IOUtils.display(IOUtils.getNickNameOrNameWithArticle(this) + " says: \"I'm the next in line to be the big Kahoona of this ship." + 
+			                                                             "\nSo I'm on my best behavior... at least while my dad is in the room..." + 
+			                                                             "\nWhat's that? ... I'm not sure how those cleaning supplies got here... you should ask my dad.\"");
+		}
+		else {
+			IOUtils.display(IOUtils.getNickNameOrNameWithArticle(this) + " says: \"Alright, ALRIGHT! YES, I took the cleaning supplies!" + 
+			                                                             "\nPlease don't tell my dad! I'm just going to sit here quietly and look at all these nifty controls...\"");
+		}
+	}
+}
